366f7e1ceec16b93c7d0bbf7b12ddf6c3da72e95
Myeongzip <shayne0914@skuniv.ac.kr>
2023-09-07 12:48:07 +0900

    todo model

--- todo/models.py
+++ todo/models.py
@@ -1,10 +1,16 @@
 from django.conf import settings
 from django.db import models
+from django.forms import DateTimeField
 
 from user.models import User
 
 
-# class Todo(models.Model):
-#     # author = models.ForeignKey(User.username,on_delete=models.CASCADE)
-#     # created_at = 
-#     # updated_at
+
+class Todo(models.Model):
+    content = models.CharField(max_length=100)
+    user = models.ForeignKey("user.User", on_delete = models.CASCADE)
+    created_at = models.DateTimeField(auto_now=False, auto_now_add=False)
+    updated_at = models.DateTimeField(auto_now=False, auto_now_add=False)
+    is_done = models.BooleanField()
+    image = models.ImageField(upload_to='images/')
+    
