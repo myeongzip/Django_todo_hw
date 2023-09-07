36d53d76f70cd4da5696880b83df6313fd1770ee
Myeongzip <shayne0914@skuniv.ac.kr>
2023-09-07 11:44:24 +0900

    add user model and startapp todo

---
+++ todo/models.py
@@ -0,0 +1,10 @@
+from django.conf import settings
+from django.db import models
+
+from user.models import User
+
+
+# class Todo(models.Model):
+#     # author = models.ForeignKey(User.username,on_delete=models.CASCADE)
+#     # created_at = 
+#     # updated_at
